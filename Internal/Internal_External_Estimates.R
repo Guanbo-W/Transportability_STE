8f15cd350984a91e3c8adc12a59f767bd489590f
Guanbo-W <gwang@hsph.harvard.edu>
2023-04-30 21:47:11 -0400

    Delete Internal directory

--- Internal/Internal_External_Estimates.R
+++
@@ -1,8 +0,0 @@
-###### To show Biases, coverage of simultaneous confidence bands, coverage of pointwise confidence intervals,theoretical standard deviation and Monte-Carlo standard deviation of for each of the subgroup effects using the propsed doubly robust estimator. 
-
-source("Data_Generation.R")
-true1=GetTrue(n,n_m,1)
-true2=GetTrue(n,n_m,2)
-true3=GetTrue(n,n_m,3)
-true4=GetTrue(n,n_m,4)
-true5=GetTrue(n,n_m,5)
